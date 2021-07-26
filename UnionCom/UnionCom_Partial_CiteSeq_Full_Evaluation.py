507a5ca10ce20f2756d62f528da7a75c3bd76230
AkashCiel <36273163+AkashCiel@users.noreply.github.com>
2021-07-26 18:05:20 +0200

    Add files via upload

---
+++ UnionCom/UnionCom_Partial_CiteSeq_Full_Evaluation.py
@@ -0,0 +1,160 @@
+"""
+    This script will:
+    1. Import required modules
+    2. Define required parameters
+    3. Load data
+    4. Perform evaluation on ALL metrics discussed in the report
+    5. Save evaluation results
+"""
+import pandas as pd
+from sklearn.metrics import silhouette_samples, silhouette_score
+from sklearn.neighbors import NearestNeighbors, KNeighborsClassifier
+import numpy as np
+import sys
+random_seed = 42
+# Define parameters
+
+dataset = "CiteSeq"
+tech_01 = "ADT"
+tech_02 = "RNA"
+label = "L1"
+sub_label = "L2"
+
+####################################################
+# MAKE SURE path suffix and path prefix are correct
+####################################################
+
+annotations_data_dir = "Unsupervised_Manifold_Alignment/Data/CiteSeq Data"
+results_data_dir = "Unsupervised_Manifold_Alignment/UnionCom/{} Evaluation Results".format(dataset)
+integrated_data_dir = "Unsupervised_Manifold_Alignment/UnionCom/{} Integrated Data".format(dataset)
+
+print("=======================================================================")
+print("Loading data . . .")
+print("=======================================================================")
+
+# Load data
+
+tech_01_projected = pd.read_csv("{}/UnionCom_Integrated_Reduced_from_50_1000_{}_Partial_Small_{}.csv".format(integrated_data_dir, random_seed, tech_01)
+, header=0, delimiter=',', index_col = 0)
+
+tech_02_projected = pd.read_csv("{}/UnionCom_Integrated_Reduced_from_50_1000_{}_Partial_Small_{}.csv".format(integrated_data_dir, random_seed, tech_02)
+, header=0, delimiter=',', index_col = 0)
+
+Annotation_Data_01 = pd.read_csv("{}/{}_annotations.csv".format(annotations_data_dir, tech_01)
+, header=0, delimiter=',', index_col = 0)
+Annotation_Data_02 = pd.read_csv("{}/{}_annotations.csv".format(annotations_data_dir, tech_02)
+, header=0, delimiter=',', index_col = 0)
+
+tech_01_projected_annotated = tech_01_projected.merge(Annotation_Data_01, how='inner', left_index=True, right_index=True)
+tech_02_projected_annotated = tech_02_projected.merge(Annotation_Data_02, how='inner', left_index=True, right_index=True)
+
+
+# Define necessary functions for evaluation
+def calc_sil(x1_mat, x2_mat, x1_lab, x2_lab):  # function to calculate Silhouette scores
+
+    x = np.concatenate((x1_mat, x2_mat))
+    lab = np.concatenate((x1_lab, x2_lab))
+
+    sil_score = silhouette_samples(x, lab)
+    avg = np.mean(sil_score)
+
+    return avg
+
+def transfer_accuracy(train_tech, test_tech, train_labels, test_labels, k):
+    knn = KNeighborsClassifier(k)
+    knn.fit(train_tech, train_labels)
+    test_label_predict = knn.predict(test_tech)
+    count = 0
+    for label1, label2 in zip(test_label_predict, test_labels):
+      if label1 == label2:
+        count += 1
+    return 100*(count / len(test_labels))
+
+def get_majority_local_category(target_tech_annotated, indices, category):
+    categories = [target_tech_annotated.iloc[[index]][category].item() for index in indices]
+    return max(categories, key=categories.count)
+
+def evaluate_first_k_neighbors(source_to_target, source_to_target_annotated,
+                               target_tech, target_tech_annotated, k,
+                               label, sub_label):
+    # Perfom knn estimation
+    neighbors = NearestNeighbors(n_neighbors=k, algorithm='auto').fit(target_tech)  # Dataset to find the neighbours in
+    all_distances, all_indices = neighbors.kneighbors(source_to_target)  # Query dataset
+
+    sub_cell_type_matches = []
+    cell_type_matches = []
+    for i in range(len(all_indices)):
+        if (i % 500) == 0:
+            print('\r', "Evaluating sample #{}".format(i), end='')
+        sub_cell_type_source = source_to_target_annotated.iloc[[i]][sub_label].item()
+        cell_type_source = source_to_target_annotated.iloc[[i]][label].item()
+
+        sub_cell_type_neighbors = get_majority_local_category(target_tech_annotated, all_indices[i], sub_label)
+        cell_type_neighbors = get_majority_local_category(target_tech_annotated, all_indices[i], label)
+
+        sub_cell_type_matches.append(1 * (sub_cell_type_source == sub_cell_type_neighbors))
+        cell_type_matches.append(1 * (cell_type_source == cell_type_neighbors))
+
+    train_labels = list(target_tech_annotated[label])
+    train_sub_labels = list(target_tech_annotated[sub_label])
+    test_labels = list(source_to_target_annotated[label])
+    test_sub_labels = list(source_to_target_annotated[sub_label])
+    label_transfer_accuracy = transfer_accuracy(target_tech, source_to_target,
+                                                train_labels, test_labels, k)
+    sub_label_transfer_accuracy = transfer_accuracy(target_tech, source_to_target,
+                                                    train_sub_labels, test_sub_labels, k)
+
+    sil1 = calc_sil(target_tech, source_to_target,
+                    train_labels, test_labels)
+    sil2 = calc_sil(source_to_target, target_tech,
+                    test_labels, train_labels)
+    silhouette_score = (sil1+sil2)/2
+
+    evaluation = {"Percentage sub cell_type matches": 100 * (sum(sub_cell_type_matches) / len(sub_cell_type_matches)),
+                  "Percentage cell type matches": 100 * (sum(cell_type_matches) / len(cell_type_matches)),
+                  "Label transfer accuracy" : label_transfer_accuracy,
+                  "Sub Label transfer accuracy" : sub_label_transfer_accuracy,
+                  "Silhouette score" : silhouette_score}
+
+    return evaluation
+
+
+Evaluation_Columns = ["Model", "k",
+                      "Percentage sub cell_type matches",
+                      "Percentage cell type matches",
+                      "Label transfer accuracy",
+                      "Sub Label transfer accuracy",
+                      "Silhouette score"]
+
+evaluation_results = []
+
+print("#########################################################")
+print("#########################################################")
+print("")
+
+k_array = [5, 50, 100]
+#k_array = [5]
+print("Total cells: ", len(tech_01_projected))
+for k in k_array:
+    print("############################################################")
+    print("")
+    print("Evaluating for k = ", k)
+    print("")
+    results = evaluate_first_k_neighbors(tech_01_projected, tech_01_projected_annotated,
+                                         tech_02_projected, tech_02_projected_annotated, k,
+                                         label, sub_label)
+    print('')
+    print(results)
+
+    evaluation_results.append(["TopoGAN", k,
+                            results["Percentage sub cell_type matches"],
+                            results["Percentage cell type matches"],
+                           results["Label transfer accuracy"],
+                           results["Sub Label transfer accuracy"],
+                           results["Silhouette score"]])
+
+# Save evaluation results
+evaluation_results = pd.DataFrame(evaluation_results, columns=Evaluation_Columns)
+evaluation_results.to_csv(
+    path_or_buf="{}/Full Evaluation Results Partial CiteSeq {}.csv".format(results_data_dir, random_seed),
+    header=True, index=False)
---
+++ UnionCom/UnionCom_Partial_PBMC.py
@@ -0,0 +1,138 @@
+"""
+    This script will:
+        1. Load data
+        2. Implement UnionCom
+        3. Save and evaluate integrated data
+"""
+
+# Import modules
+import pandas as pd
+import torchvision
+from unioncom import UnionCom
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+import torch
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+import sys
+
+random_seed = int(sys.argv[1])
+print("Random seed : {}".format(random_seed))
+
+# Define parameters
+source_tech_name = "ATAC"
+epochs = 1000
+path_suffix = "Reduced_from_50_{}_{}_Partial".format(epochs, random_seed)
+path_prefix = "Unsupervised_Manifold_Alignment/UnionCom"
+data_dir = "Unsupervised_Manifold_Alignment/Data/PBMC Data"
+
+print("=======================================================================")
+print("Loading data . . .")
+print("=======================================================================")
+
+# Load data from all domains
+if source_tech_name == "RNA":
+  target_tech_name = "ATAC"
+
+else:
+  target_tech_name = "RNA"
+
+source_tech = pd.read_csv("{}/{} Processed Reduced Partial.csv".format(data_dir, source_tech_name), header=0, delimiter=',', index_col = 0)
+
+target_tech = pd.read_csv("{}/{} Processed Reduced Partial.csv".format(data_dir, target_tech_name), header=0, delimiter=',', index_col = 0)
+
+source_indices = list(source_tech.index)
+target_indices = list(target_tech.index)
+
+Annotation_Data_Source = pd.read_csv("{}/Annotations_{}.csv".format(data_dir, source_tech_name)
+, header=0, delimiter=',', index_col = 0)
+Annotation_Data_Target = pd.read_csv("{}/Annotations_{}.csv".format(data_dir, target_tech_name)
+, header=0, delimiter=',', index_col = 0)
+
+# Add source labels
+Annotation_Data_Source["source"] = Annotation_Data_Source.apply(lambda row: source_tech_name, axis=1)
+Annotation_Data_Target["source"] = Annotation_Data_Target.apply(lambda row: target_tech_name, axis=1)
+
+# Append Annotation Data
+Annotation_Integrated = Annotation_Data_Target.append(Annotation_Data_Source)
+
+source_tech = source_tech.to_numpy()
+source_tech = source_tech.astype(float)
+print("Source Technology: ", np.shape(source_tech))
+target_tech = target_tech.to_numpy()
+target_tech = target_tech.astype(float)
+print("Target Technology: ", np.shape(target_tech))
+
+uc = UnionCom.UnionCom(epoch_pd=epochs, epoch_DNN=200, manual_seed=random_seed, output_dim=8)
+integrated_data = uc.fit_transform(dataset=[source_tech,target_tech])
+
+tech_01_projected = pd.DataFrame(integrated_data[0], index=source_indices).astype("float")
+tech_02_projected = pd.DataFrame(integrated_data[1], index=target_indices).astype("float")
+
+# Annotate data
+tech_01_projected_annotated = tech_01_projected.merge(Annotation_Data_Source, how='inner', left_index=True, right_index=True)
+tech_02_projected_annotated = tech_02_projected.merge(Annotation_Data_Target, how='inner', left_index=True, right_index=True)
+
+# Write to file
+tech_01_projected.to_csv(path_or_buf="{}/PBMC Integrated Data/UnionCom_Integrated_{}_{}.csv".format(path_prefix, path_suffix, source_tech_name), header=True, index=True)
+tech_02_projected.to_csv(path_or_buf="{}/PBMC Integrated Data/UnionCom_Integrated_{}_{}.csv".format(path_prefix, path_suffix, target_tech_name), header=True, index=True)
+
+def get_majority_local_category(tech_02_projected_annotated, indices, category):
+    categories = [tech_02_projected_annotated.iloc[[index]][category].item() for index in indices]
+    return max(categories, key=categories.count)
+
+def evaluate_first_k_neighbors(tech_01_projected, tech_01_projected_annotated,
+                               tech_02_projected, tech_02_projected_annotated, k):
+    # Perfom knn estimation
+    neighbors = NearestNeighbors(n_neighbors=k, algorithm='auto').fit(tech_02_projected)  # Dataset to find the neighbours in
+    all_distances, all_indices = neighbors.kneighbors(tech_01_projected)  # Query dataset
+
+    sub_cell_type_matches = []
+    cell_type_matches = []
+    for i in range(len(all_indices)):
+        if (i % 500) == 0:
+            print('\r', "Evaluating sample #{}".format(i), end='')
+        sub_cell_type_source = tech_01_projected_annotated.iloc[[i]]["cell_type_00"].item()
+        cell_type_source = tech_01_projected_annotated.iloc[[i]]["cell_type"].item()
+
+        sub_cell_type_neighbors = get_majority_local_category(tech_02_projected_annotated, all_indices[i], "cell_type_00")
+        cell_type_neighbors = get_majority_local_category(tech_02_projected_annotated, all_indices[i], "cell_type")
+
+        sub_cell_type_matches.append(1 * (sub_cell_type_source == sub_cell_type_neighbors))
+        cell_type_matches.append(1 * (cell_type_source == cell_type_neighbors))
+
+    evaluation = {"Percentage sub cell_type matches": 100 * (sum(sub_cell_type_matches) / len(sub_cell_type_matches)),
+                  "Percentage cell type matches": 100 * (sum(cell_type_matches) / len(cell_type_matches))}
+
+    return evaluation
+
+
+Evaluation_Columns = ["Model", "Tech 01", "Tech 02", "k",
+                      "Percentage sub cell_type matches",
+                      "Percentage cell type matches"]
+
+evaluation_results = []
+
+#k_array = [1, 5, 10, 15, 20, 50, 100, 200]
+k_array = [5]
+
+print("Total cells: ", len(tech_01_projected))
+for k in k_array:
+    print("############################################################")
+    print("")
+    print("Evaluating for k = ", k)
+    print("")
+    results = evaluate_first_k_neighbors(tech_01_projected, tech_01_projected_annotated,
+                                         tech_02_projected, tech_02_projected_annotated,
+                                         k)
+    print('')
+    print(results)
+
+    evaluation_results.append(["UnionCom", source_tech_name, target_tech_name, k,
+                               results["Percentage sub cell_type matches"],
+                               results["Percentage cell type matches"]])
+
+# Save evaluation results
+evaluation_results = pd.DataFrame(evaluation_results, columns=Evaluation_Columns)
+evaluation_results.to_csv(
+    path_or_buf="{}/PBMC Evaluation Results/Evaluation Results_{}.csv".format(path_prefix, path_suffix),
+    header=True, index=False)
