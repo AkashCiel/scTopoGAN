e10f4d64804d9baa343c68e594e1c775db51690f
Tamim Abdelaal <31687847+tabdelaal@users.noreply.github.com>
2023-01-25 14:20:48 +0100

    Remove old implementation

--- scTopoGAN/src/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Datasets."""
-from .manifolds import Spheres
-#from .mnist import MNIST
-#from .fashion_mnist import FashionMNIST
-#from .cifar10 import CIFAR
-#__all__ = ['Spheres', 'MNIST', 'FashionMNIST', 'CIFAR']
-__all__ = ['Spheres']
